dced9e643269a061124cbb47b265bb6e6aeb66ed
ingswgruppo13 <progingsw2020@gmail.com>
2020-02-03 16:37:48 +0100

    Aggiunte funzionalità di login di Firebase

--- app/src/main/java/com/e/cv_19/Activity_login.java
+++ app/src/main/java/com/e/cv_19/Activity_login.java
@@ -1,31 +1,84 @@
 package com.e.cv_19;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class Activity_login extends AppCompatActivity {
+
+    private static final String TAG = "Activity Login";
+    private FirebaseAuth mAuth;
     private EditText campo_email;
     private EditText campo_password;
 
     @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
-        campo_email = findViewById(R.id.Editviewemail);
-        campo_password = findViewById(R.id.editTextpassword);
     }
 
     public void Login_is_clicked(View view) {
 
-        /*per ottenere i dati dalle EditText usare getText().toString() su campo_email e password*/
+        campo_email = findViewById(R.id.Editviewemail);
+        campo_password = findViewById(R.id.editTextpassword);
+
+        String email=campo_email.getText().toString();
+        String password=campo_password.getText().toString();
+
 
     }
 
+    private void loginUser(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Activity_login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+
+    }
     public void Registrati_is_clicked(View view) {
 
     }
