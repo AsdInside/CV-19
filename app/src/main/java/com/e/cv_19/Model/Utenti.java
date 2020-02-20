6e1d2cdb12ecd7e8b09d41eb6c740850a5a4173c
ingswgruppo13 <progingsw2020@gmail.com>
2020-02-20 17:38:01 +0100

    mod

---
+++ app/src/main/java/com/e/cv_19/Model/Utenti.java
@@ -0,0 +1,45 @@
+package com.e.cv_19.Model;
+
+public class Utenti {
+    private String nome;
+    private String cognome;
+    private String nickname;
+    private String idUtente;
+
+
+    public Utenti() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getIdUtente() {
+        return idUtente;
+    }
+
+    public void setIdUtente(String idUtente) {
+        this.idUtente = idUtente;
+    }
+}
